ad42217df984c3709b13772d532e1f88b9b65f17
Tobiewe <tobiewe02@gmail.com>
2023-04-04 19:10:58 +0200

    DAODepartamentoimp acabado

--- Hotel-IS2-cod/src/Integracion/Departamentos/DAODepartamentosImp.java
+++ Hotel-IS2-cod/src/Integracion/Departamentos/DAODepartamentosImp.java
@@ -45,23 +45,100 @@ public class DAODepartamentosImp implements DAODepartamentos {
 	
 	
 	public Integer borrar(Integer id) {
-		// TODO Auto-generated method stub
-		return null;
+		int key = -1;
+
+		try {
+			String consulta = "UPDATE hotel-is2.departamento SET activo = ? WHERE Id = ?;";
+
+			Connection miConexion = DriverManager.getConnection(url, usuario, clave);
+			PreparedStatement ps = miConexion.prepareStatement(consulta);
+
+			ps.setBoolean(1, false);
+			ps.setInt(2, id);
+
+			key = (ps.executeUpdate() == 1) ? id : -1;
+			
+			miConexion.close();
+			ps.close();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+
+		return key;
 	}
-	@Override
+	
+	
 	public Integer modificar(TDepartamento tDepartamento) {
-		// TODO Auto-generated method stub
-		return null;
+		int ok = -1;
+		try {
+			String consulta = "UPDATE hotel-is2.departamento SET jefe = ?, nombre = ?, activo= ? WHERE Id = ?;";
+
+			Connection miConexion = DriverManager.getConnection(url, usuario, clave);
+			PreparedStatement ps = miConexion.prepareStatement(consulta);
+
+			ps.setString(1, tDepartamento.getJefe());
+			ps.setString(2, tDepartamento.getNombre());
+			ps.setBoolean(3, tDepartamento.getActivado());
+			ps.setInt(4, tDepartamento.getId());
+			if(ps.executeUpdate()==1) ok= tDepartamento.getId();
+			
+			miConexion.close();
+			ps.close();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+
+		return ok;
 	}
-	@Override
+	
+	
 	public TDepartamento MostrarUno(Integer id) {
-		// TODO Auto-generated method stub
-		return null;
+		TDepartamento tDepartamento = null;
+		try {
+			String consulta = "SELECT * FROM hotel-is2.departamento WHERE Id = ?;";
+
+			Connection miConexion = DriverManager.getConnection(url, usuario, clave);
+			PreparedStatement ps = miConexion.prepareStatement(consulta);
+
+			ps.setInt(1, id);
+			ResultSet miResult = ps.executeQuery();
+
+			if (miResult.next()){
+				tDepartamento = new TDepartamento(miResult.getInt("Id"), miResult.getString("jefe"),
+						miResult.getString("nombre"), miResult.getBoolean("activo"));
+			}
+	
+			miConexion.close();
+			ps.close();
+			miResult.close();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return tDepartamento;
 	}
-	@Override
+
+
 	public Collection<TDepartamento> MostrarTodos() {
-		// TODO Auto-generated method stub
-		return null;
+		
+		ArrayList<TDepartamento> lista = new ArrayList<TDepartamento>();
+		try {
+			String consulta = "SELECT * FROM hotel-is2.departamento";
+
+			Connection miConexion = DriverManager.getConnection(url, usuario, clave);
+			Statement miStatement = miConexion.createStatement();
+			ResultSet miResult = miStatement.executeQuery(consulta);
+
+			while (miResult.next())
+				lista.add(new TDepartamento(miResult.getInt("Id"), miResult.getString("jefe"),
+						miResult.getString("nombre"), miResult.getBoolean("activo")));
+			miConexion.close();
+			miStatement.close();
+			miResult.close();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return lista;
+		
 	}
 	
 
