d8da3a27d06e1ba70bc001c2018298402131a0be
Tobiewe <tobiewe02@gmail.com>
2023-04-04 19:41:02 +0200

    Trasfer Habitacion

--- Hotel-IS2-cod/src/Negocio/Habitaciones/THabitaciones.java
+++ Hotel-IS2-cod/src/Negocio/Habitaciones/THabitaciones.java
@@ -1,5 +1,60 @@
 package Negocio.Habitaciones;
 
 public class THabitaciones {
+	
+	private Integer numero;
+	private Integer piso;
+	private Float tamaño;
+	private Float precio;
+	private Boolean ocupada;
+	private Integer id_empleado;
+	
+	//this.numero = numero;
+	//this.piso = piso;
+	//this.tamaño = tamaño;
+	//this.precio = precio;
+	//this.ocupada = ocupada;
+	//this.id_empleado = id_empleado;
+	
+	public THabitaciones(Integer numero, Integer piso, Float tamaño, Float precio, Boolean ocupada, Integer id_empleado){
+		
+		this.numero = numero;
+		this.piso = piso;
+		this.tamaño = tamaño;
+		this.precio = precio;
+		this.ocupada = ocupada;
+		this.id_empleado = id_empleado;
+		
+	}
 
+	public Integer getNumero() {
+		return numero;
+	}
+
+	public Integer getPiso() {
+		return piso;
+	}
+
+	public Float getTamaño() {
+		return tamaño;
+	}
+
+	public Float getPrecio() {
+		return precio;
+	}
+
+	public void setPrecio(Float precio) {
+		this.precio = precio;
+	}
+
+	public Boolean getOcupada() {
+		return ocupada;
+	}
+
+	public void setOcupada(Boolean ocupada) {
+		this.ocupada = ocupada;
+	}
+
+	
+	
 }
