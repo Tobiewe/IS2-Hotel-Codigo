dfe59ce7e1dcc64ac63a4c45104053f2aa48a188
Tobiewe <tobiewe02@gmail.com>
2023-04-05 13:25:28 +0200

    TReserva y lineapedido acabados

--- Hotel-IS2-cod/src/Negocio/Reserva/TLineaPedido.java
+++ Hotel-IS2-cod/src/Negocio/Reserva/TLineaPedido.java
@@ -1,5 +1,40 @@
 package Negocio.Reserva;
 
+
 public class TLineaPedido {
+	
+	private Integer id_reserva;
+	private Integer id_cliente;
+	private Integer id_habitacion;
+	private Boolean activa;
+	
+	public TLineaPedido(Integer id_reserva, Integer id_cliente, Integer id_habitacion, Boolean activa){
+		
+		this.id_reserva = id_reserva;
+		this.id_cliente = id_cliente;
+		this.id_habitacion = id_habitacion;
+		this.activa = activa;
+		
+	}
+
+	public Integer getId_reserva() {
+		return id_reserva;
+	}
+
+	public Integer getId_cliente() {
+		return id_cliente;
+	}
+
+	public Integer getId_habitacion() {
+		return id_habitacion;
+	}
+
+	public Boolean getActiva() {
+		return activa;
+	}
+
+	public void setActiva(Boolean activa) {
+		this.activa = activa;
+	}
 
 }
