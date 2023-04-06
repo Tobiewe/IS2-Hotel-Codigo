08644522096f99f0522cd5f066a29ac497b7e852
JavierRDA01 <javier.ramirezdeandres34439@gmail.com>
2023-04-06 11:43:06 +0200

    cambios Factoria de integracion e inicio SACliente

--- Hotel-IS2-cod/src/Integracion/FactoriaIntegracion/FactoriaIntegracion.java
+++ Hotel-IS2-cod/src/Integracion/FactoriaIntegracion/FactoriaIntegracion.java
@@ -1,5 +1,13 @@
 package Integracion.FactoriaIntegracion;
 
-public class FactoriaIntegracion {
-
+public  class FactoriaIntegracion {
+		
+	private static FactoriaIntegracion instance;
+	
+	public static FactoriaIntegracion getInstance()
+	{
+		if (instance == null)
+			instance = new FactoriaIntegracionImp();
+		return instance;
+	}
 }
--- Hotel-IS2-cod/src/Integracion/FactoriaIntegracion/FactoriaIntegracionImp.java
+++ Hotel-IS2-cod/src/Integracion/FactoriaIntegracion/FactoriaIntegracionImp.java
@@ -1,5 +1,34 @@
 package Integracion.FactoriaIntegracion;
 
-public abstract class FactoriaIntegracionImp {
 
+import Integracion.Cliente.DAOCliente;
+import Integracion.Cliente.DAOClienteImp;
+import Integracion.Departamentos.DAODepartamentos;
+import Integracion.Departamentos.DAODepartamentosImp;
+import Integracion.Habitaciones.DAOHabitaciones;
+import Integracion.Habitaciones.DAOHabitacionesImp;
+import Integracion.Tareas.DAOTareas;
+import Integracion.Tareas.DAOTareasImp;
+public class FactoriaIntegracionImp  extends FactoriaIntegracion{
+	
+	public DAOHabitaciones newDAOEmpleado()
+	{
+		return new DAOHabitacionesImp();
+	}
+	
+	public DAOTareas newDAOTareas()
+	{
+		return new DAOTareasImp();
+	}
+	
+	public DAODepartamentos newDAODepartamentos()
+	{
+		return new DAODepartamentosImp();
+	}
+	
+	public DAOCliente newDAOClientes()
+	{
+		return new DAOClienteImp();
+	}
+		
 }
