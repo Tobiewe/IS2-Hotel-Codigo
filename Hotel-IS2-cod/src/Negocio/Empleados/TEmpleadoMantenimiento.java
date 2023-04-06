2d7c89dff10d72df3c63c9bac3da8edcea4f351b
Tobiewe <tobiewe02@gmail.com>
2023-04-06 16:45:26 +0200

    TEmpleado corregidos

--- Hotel-IS2-cod/src/Negocio/Empleados/TEmpleadoMantenimiento.java
+++ Hotel-IS2-cod/src/Negocio/Empleados/TEmpleadoMantenimiento.java
@@ -2,13 +2,16 @@ package Negocio.Empleados;
 
 public class TEmpleadoMantenimiento extends TEmpleados {
 
-	public TEmpleadoMantenimiento(Integer id, Float sueldo, String nombre, String apellidos, Boolean activo,
-			String correo, Integer telefono, String especialidad) {
-		
-		super(id, sueldo, nombre, apellidos, activo, correo, telefono, null, especialidad);
 
-	}
 	
+	public TEmpleadoMantenimiento(Integer id, Float sueldo, String nombre, String apellidos, Boolean activo,
+			String correo, Integer telefono, String lugar, String especialidad, Integer id_Departamento) {
+		
+		super(id, sueldo, nombre, apellidos, activo, correo, telefono, null, especialidad, id_Departamento);
+		// TODO Auto-generated constructor stub
+	}
+
+
 	public String getEspecialidad() {
 		return especialidad;
 	}
