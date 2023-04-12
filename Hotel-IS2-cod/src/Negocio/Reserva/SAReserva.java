30ceba7ccce92e579da8aa446aa7c5f2abea6d22
Tobiewe <tobiewe02@gmail.com>
2023-04-12 19:00:33 +0200

    cambios un poco en todo y sareserva acabado a falta de cerrar

--- Hotel-IS2-cod/src/Negocio/Reserva/SAReserva.java
+++ Hotel-IS2-cod/src/Negocio/Reserva/SAReserva.java
@@ -3,25 +3,25 @@ package Negocio.Reserva;
 import java.util.Collection;
 
 import Negocio.Reserva.TReserva;
-import Negocio.Reserva.TLineaPedido;
+import Negocio.Reserva.TLineaReserva;
 
 public interface SAReserva {
 	public Integer abrir(TReserva reserva);
 	
-	public Integer aniadirHabitacion(TLineaPedido pedido);
+	public Integer aniadirHabitacion(TLineaReserva pedido);
 	
-	public Integer eliminarHabitacion(TLineaPedido pedido);
+	public Integer eliminarHabitacion(TLineaReserva pedido);
 	
 	public Integer modificar(TReserva reserva);
 	
-	public Integer cerrar(TLineaPedido pedido);
+	public Integer cerrar(TLineaReserva pedido);
 
-	public Integer eliminar(int idReserva);
+	public Integer eliminar(Integer idReserva);
 
-	public TReserva mostrarUno(int idReserva);
+	public TReserva mostrarUno(Integer idReserva);
 	
 	public Collection<TReserva> mostrarTodos();
 	
-	public Collection<TReserva> mostrarPorCliente();
+	public Collection<TReserva> mostrarPorCliente(Integer idCliente);
 	
 }
--- Hotel-IS2-cod/src/Negocio/Reserva/SAReservaIMP.java
+++ Hotel-IS2-cod/src/Negocio/Reserva/SAReservaIMP.java
@@ -1,61 +1,125 @@
 package Negocio.Reserva;
 
+import java.util.ArrayList;
 import java.util.Collection;
+
+import Integracion.FactoriaIntegracion.FactoriaIntegracion;
+import Integracion.Habitaciones.DAOHabitaciones;
+import Integracion.LineaReserva.DAOLineaReserva;
+import Integracion.Reserva.DAOReserva;
+import Negocio.Habitaciones.THabitaciones;
+import Negocio.Tareas.TTareas;
 
 public class SAReservaIMP implements SAReserva{
 
 	
 	public Integer abrir(TReserva reserva) {
-		// TODO Auto-generated method stub
-		return null;
+		if(reserva.getNoches() <= 0 || reserva.getNombre().trim().equals("")){
+			return -5;
+		}
+		
+		DAOReserva daoR = FactoriaIntegracion.getInstance().newDAOReserva();
+	
+		return daoR.abrir(reserva);
 	}
 
-	@Override
-	public Integer aniadirHabitacion(TLineaPedido pedido) {
-		// TODO Auto-generated method stub
-		return null;
+	
+	public Integer aniadirHabitacion(TLineaReserva pedido) {
+		
+		DAOHabitaciones daoH = FactoriaIntegracion.getInstance().newDAOHabitaciones();
+		THabitaciones th = daoH.MostrarUna(pedido.getId_habitacion());
+		
+		if(th.getOcupada() == true){
+			return -2;
+		}
+		
+		DAOLineaReserva daoR = FactoriaIntegracion.getInstance().newLineaReserva();
+		
+		return daoR.crear(pedido);
+		
 	}
 
-	@Override
-	public Integer eliminarHabitacion(TLineaPedido pedido) {
-		// TODO Auto-generated method stub
-		return null;
+	
+	public Integer eliminarHabitacion(TLineaReserva pedido) {		
+		DAOLineaReserva daoR = FactoriaIntegracion.getInstance().newLineaReserva();
+		
+		return daoR.eliminar(pedido.getId_reserva(), pedido.getId_cliente(), pedido.getId_habitacion());
 	}
 
-	@Override
+	
 	public Integer modificar(TReserva reserva) {
-		// TODO Auto-generated method stub
-		return null;
+		DAOReserva daoR = FactoriaIntegracion.getInstance().newDAOReserva();
+		TReserva tr = daoR.MostrarUna(reserva.getId());
+		
+		if(tr == null){
+			return -2;
+		}
+		
+		return daoR.modificar(reserva);
+		
 	}
 
-	@Override
-	public Integer cerrar(TLineaPedido pedido) {
-		// TODO Auto-generated method stub
-		return null;
+	
+	public Integer cerrar(TLineaReserva pedido) {
+		DAOLineaReserva daoR = FactoriaIntegracion.getInstance().newLineaReserva();
+		
+		return 0; //hay que mirarlo
 	}
 
-	@Override
-	public Integer eliminar(int idReserva) {
-		// TODO Auto-generated method stub
-		return null;
+	
+	public Integer eliminar(Integer idReserva) {
+		DAOReserva daoR = FactoriaIntegracion.getInstance().newDAOReserva();
+		
+		return daoR.eliminar(idReserva);
 	}
 
-	@Override
-	public TReserva mostrarUno(int idReserva) {
-		// TODO Auto-generated method stub
-		return null;
+	
+	public TReserva mostrarUno(Integer idReserva) {
+		
+		DAOReserva daoR = FactoriaIntegracion.getInstance().newDAOReserva();
+		TReserva tr = daoR.MostrarUna(idReserva);
+		
+		if(tr == null){
+			return null;
+		}
+		
+		return tr;
 	}
 
-	@Override
+	
 	public Collection<TReserva> mostrarTodos() {
-		// TODO Auto-generated method stub
-		return null;
+		
+		DAOReserva daoR = FactoriaIntegracion.getInstance().newDAOReserva();
+		Collection<TReserva> lista = daoR.MostrarTodas();
+		Collection<TReserva> dev = new ArrayList<TReserva>();
+		
+		for(TReserva t : lista){
+			TReserva tr = daoR.MostrarUna(t.getId());
+			if(tr != null){
+				dev.add(t);
+			}
+		}
+		
+		return dev;
+		
 	}
 
-	@Override
-	public Collection<TReserva> mostrarPorCliente() {
-		// TODO Auto-generated method stub
-		return null;
+	
+	public Collection<TReserva> mostrarPorCliente(Integer idCliente) {
+		
+		DAOReserva daoR = FactoriaIntegracion.getInstance().newDAOReserva();
+		Collection<TReserva> lista = daoR.leerReservasPorCliente(idCliente);
+		Collection<TReserva> dev = new ArrayList<TReserva>();
+		
+		for(TReserva t : lista){
+			TReserva tr = daoR.MostrarUna(t.getId());
+			if(tr != null){
+				dev.add(t);
+			}
+		}
+		
+		return dev;
+		
 	}
 
 }
