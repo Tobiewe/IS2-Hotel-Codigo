59fba2166f5669e77eb4aa3be00996b48ff1771b
hugag <aguilerahugo28@gmail.com>
2023-04-14 13:15:23 +0200

    abrir reserva, falta el total¿?

--- Hotel-IS2-cod/src/Presentacion/Reserva/VReservaCasosUso/VAbrirReserva.java
+++ Hotel-IS2-cod/src/Presentacion/Reserva/VReservaCasosUso/VAbrirReserva.java
@@ -1,11 +1,128 @@
 package Presentacion.Reserva.VReservaCasosUso;
 
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 
+import java.awt.Dimension;
+import java.awt.FlowLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.*;
+import javax.swing.event.ChangeEvent;
+import javax.swing.event.ChangeListener;
+import java.util.Calendar;
+import java.util.Date;
+import javax.swing.BoxLayout;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JSpinner;
+import javax.swing.JTable;
+import javax.swing.SpinnerNumberModel;
+import javax.swing.SwingUtilities;
+import javax.swing.border.TitledBorder;
+import javax.swing.event.ChangeEvent;
+import javax.swing.event.ChangeListener;
+import javax.swing.table.AbstractTableModel;
+
+import Negocio.Reserva.TReserva;
+import Presentacion.Controller.Controller;
+import Presentacion.Controller.Events;
+import Presentacion.Controller.IGUI;
 import Presentacion.Controller.IGUI;
 
 public class VAbrirReserva extends JFrame implements IGUI {
 
+	private Controller ctrl;
+	private String title = "Abrir Reserva";
+	private Integer noches;
+	private Date fecha;
+	public VAbrirReserva(){
+		ctrl = Controller.getInstance();
+		SwingUtilities.invokeLater(new Runnable() {
+			@Override
+			public void run() {
+				initGUI();
+			}
+		});
+	}
+	
+	protected void initGUI() {
+	    JPanel mainPanel = new JPanel();
+	    mainPanel.setLayout(new BoxLayout(mainPanel, BoxLayout.Y_AXIS));
+	    setContentPane(mainPanel);
+	    setTitle(title);
+
+	    JPanel nochesPanel = new JPanel(new FlowLayout(FlowLayout.LEFT));
+	    nochesPanel.add(new JLabel("Noches: "));
+	    nochesPanel.add(nochesSpinner());
+
+	    JPanel fechaPanel = new JPanel(new FlowLayout(FlowLayout.LEFT));
+	    fechaPanel.add(new JLabel("Fecha: "));
+	    fechaPanel.add(fechaSpinner());
+
+	    JPanel cancelButtonPanel = new JPanel(new FlowLayout(FlowLayout.CENTER));
+	    cancelButtonPanel.add(cancelButton());
+
+	    mainPanel.add(nochesPanel);
+	    mainPanel.add(fechaPanel);
+	    mainPanel.add(cancelButtonPanel);
+
+	    pack();
+	    setLocationRelativeTo(null);
+	    setVisible(true);
+	}
+	
+	public JSpinner nochesSpinner(){
+		JSpinner nochesSpin = new JSpinner(new SpinnerNumberModel(1,1,Integer.MAX_VALUE,1));
+		nochesSpin.setPreferredSize(new Dimension(40, 15));
+		noches = (Integer) nochesSpin.getValue();
+		nochesSpin.addChangeListener(new ChangeListener()
+		{
+
+			@Override
+			public void stateChanged(ChangeEvent e) {
+				noches = (Integer) nochesSpin.getValue();
+			}
+			
+		});
+		
+		return nochesSpin;
+		
+	}
+	public JSpinner fechaSpinner() {
+	    JSpinner fechaSpin = new JSpinner(new SpinnerDateModel(new Date(), null, null, Calendar.DAY_OF_MONTH));
+	    fechaSpin.setEditor(new JSpinner.DateEditor(fechaSpin, "dd/MM/yyyy"));
+	    fechaSpin.setPreferredSize(new Dimension(100, 20));
+	    fecha = (Date) fechaSpin.getValue();
+	    fechaSpin.addChangeListener(new ChangeListener() {
+	        @Override
+	        public void stateChanged(ChangeEvent e) {
+	            fecha = (Date) fechaSpin.getValue();
+	        }
+	    });
+
+	    return fechaSpin;
+	}
+
+	public JButton cancelButton()
+	{
+		JButton cancelButton = new JButton("Cancel");
+		cancelButton.addActionListener(new ActionListener()
+		{
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				setVisible(false);
+				ctrl.carryAction(Events.RESERVA_NUEVA_VISTA, null);
+			}
+		
+		});
+		return cancelButton;
+	}
+	
+	
 	@Override
 	public void update(int event, Object datos) {
 		// TODO Auto-generated method stub
