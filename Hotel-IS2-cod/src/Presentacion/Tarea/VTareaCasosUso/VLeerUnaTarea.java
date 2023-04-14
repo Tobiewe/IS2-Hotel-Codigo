6c91fe53a7c20e4ab347e7312e56b3d28a06f519
JavierRDA01 <javier.ramirezdeandres34439@gmail.com>
2023-04-14 16:20:49 +0200

    Para que se actualice

--- Hotel-IS2-cod/src/Presentacion/Tarea/VTareaCasosUso/VLeerUnaTarea.java
+++ Hotel-IS2-cod/src/Presentacion/Tarea/VTareaCasosUso/VLeerUnaTarea.java
@@ -5,7 +5,7 @@ import javax.swing.JFrame;
 import Presentacion.Controller.IGUI;
 
 public class VLeerUnaTarea extends JFrame implements IGUI{
-
+//s
 	@Override
 	public void update(int event, Object datos) {
 		// TODO Auto-generated method stub
